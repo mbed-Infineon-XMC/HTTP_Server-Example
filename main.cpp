ce3ec4a5ddf0f63cc95426669e8ea520198bce42
hackdino <franzney14@gmail.com>
2018-02-13 00:47:13 +0100

    Add ethernet Example

--- main.cpp
+++ main.cpp
@@ -1,64 +1,38 @@
-/* mbed Example Program
- * Copyright (c) 2006-2014 ARM Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/******************************************************************* Includes */
 #include "mbed.h"
-#include "rtos.h"
+#include "EthernetInterface.h"
 
-/******************************************************************** Globals */
-AnalogIn input(P14_0);
-DigitalOut led_flash(LED1);
-Serial device(P0_5, P0_4); // tx, rx
+// Network interface
+EthernetInterface net;
 
-Thread thread1;
-
-/****************************************************************** Functions */
-
-/**
- * ADC Thread
- */
-void adc_thread() {
-
-    float vin;
-
-    /* Initialize Serial */
-    device.baud(9600);
-    device.printf("ADC-Example\n");
-
-    /* Print input voltage */
-    while(1){
-        wait(1);
-        vin = (input.read() * 3.3);
-        device.printf("Vin = %.01f V\n", vin);
-    }
-}
-
-/**
- * Main Function
- */
+// Socket demo
 int main() {
+    // Bring up the ethernet interface
+    printf("Ethernet socket example\n");
+    net.connect();
 
-    /* Start ADC-Sample Thread */
-    thread1.start(adc_thread);
+    // Show the network address
+    const char *ip = net.get_ip_address();
+    printf("IP address is: %s\n", ip ? ip : "No IP");
 
-    /* Toggle LED1  */
-    while (1) {
-        led_flash = !led_flash;
-        wait(1);
-    }
+    // Open a socket on the network interface, and create a TCP connection to mbed.org
+    TCPSocket socket;
+    socket.open(&net);
+    socket.connect("developer.mbed.org", 80);
+
+    // Send a simple http request
+    char sbuffer[] = "GET / HTTP/1.1\r\nHost: developer.mbed.org\r\n\r\n";
+    int scount = socket.send(sbuffer, sizeof sbuffer);
+    printf("sent %d [%.*s]\n", scount, strstr(sbuffer, "\r\n")-sbuffer, sbuffer);
+
+    // Recieve a simple http response and print out the response line
+    char rbuffer[64];
+    int rcount = socket.recv(rbuffer, sizeof rbuffer);
+    printf("recv %d [%.*s]\n", rcount, strstr(rbuffer, "\r\n")-rbuffer, rbuffer);
+
+    // Close the socket to return its memory and bring down the network interface
+    socket.close();
+
+    // Bring down the ethernet interface
+    net.disconnect();
+    printf("Done\n");
 }
-
-/*EOF*/
